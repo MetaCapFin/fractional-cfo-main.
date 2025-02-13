e9a7e4a26d3d5c07e8f41f8194e0080842cdc944
MetaCapFin <advisor@metacap.finance>
2025-02-13 15:04:48 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -20,17 +20,19 @@ const Home = () => (
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     </Head>
     <Header />
-    <SubHeader />
-    <GifPlaceholder />
-    <CatchphraseContainer />
-    <SideBySideContainer />
-    <MissionContainer />
-    <StatisticsContainer />
-    <a href="#schedule-meeting" className="schedule-button">SCHEDULE A MEETING</a>
-    <FractionalCFOContainer />
-    <NewContainer />
-    <KeyValuesContainer />
-    <AdditionalContainer />
+    <main>
+      <SubHeader />
+      <GifPlaceholder />
+      <CatchphraseContainer />
+      <SideBySideContainer />
+      <MissionContainer />
+      <StatisticsContainer />
+      <a href="#schedule-meeting" className="schedule-button">SCHEDULE A MEETING</a>
+      <FractionalCFOContainer />
+      <NewContainer />
+      <KeyValuesContainer />
+      <AdditionalContainer />
+    </main>
     <Footer />
   </div>
 );
