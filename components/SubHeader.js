367e5773c5aaf3ec0fc73f8bd72375e2d1613814
MetaCapFin <advisor@metacap.finance>
2025-02-13 11:44:40 -0500

    Create SubHeader.js

---
+++ components/SubHeader.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const SubHeader = () => (
+  <div className="sub-header" style={{ marginTop: '70px' }}>
+    <div className="logo-placeholder">LOGO HERE</div>
+    <h1>BRAND NAME</h1>
+  </div>
+);
+
+export default SubHeader;
