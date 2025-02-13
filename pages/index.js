be095c8e4e5750f0470753c92f4f4addd428d1ed
MetaCapFin <advisor@metacap.finance>
2025-02-13 11:48:09 -0500

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Head from 'next/head';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import SubHeader from '../components/SubHeader';
+import GifPlaceholder from '../components/GifPlaceholder';
+import CatchphraseContainer from '../components/CatchphraseContainer';
+import SideBySideContainer from '../components/SideBySideContainer';
+import MissionContainer from '../components/MissionContainer';
+import StatisticsContainer from '../components/StatisticsContainer';
+import FractionalCFOContainer from '../components/FractionalCFOContainer';
+import NewContainer from '../components/NewContainer';
+import KeyValuesContainer from '../components/KeyValuesContainer';
+import AdditionalContainer from '../components/AdditionalContainer';
+
+const Home = () => (
+  <div>
+    <Head>
+      <title>Landing Page</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    </Head>
+    <Header />
+    <SubHeader />
+    <GifPlaceholder />
+    <CatchphraseContainer />
+    <SideBySideContainer />
+    <MissionContainer />
+    <StatisticsContainer />
+    <a href="#schedule-meeting" className="schedule-button">SCHEDULE A MEETING</a>
+    <FractionalCFOContainer />
+    <NewContainer />
+    <KeyValuesContainer />
+    <AdditionalContainer />
+    <Footer />
+  </div>
+);
+
+export default Home;
