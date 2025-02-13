561f77dbfbdb2ff65f97f24380f8e009a90a9c8d
MetaCapFin <advisor@metacap.finance>
2025-02-13 11:46:01 -0500

    Create StatisticsContainer.js

---
+++ components/StatisticsContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const StatisticsContainer = () => (
+  <div className="statistics-container">
+    <div className="stat-container">STATISTICS</div>
+    <div className="stat-container">STATISTICS</div>
+    <div className="stat-container">STATISTICS</div>
+    <div className="stat-container">STATISTICS</div>
+  </div>
+);
+
+export default StatisticsContainer;
