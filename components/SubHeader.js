56806dfe0f45efbbbd90b85e2108ea8794890694
MetaCapFin <advisor@metacap.finance>
2025-02-13 16:58:44 -0500

    Update SubHeader.js

--- components/SubHeader.js
+++ components/SubHeader.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import Image from 'next/image';
 
-const SubHeader = () => (
-  <div className="sub-header">
-    <div className="logo-container">
-      <Image src="/Fractional CFO Logo.png" alt="Fractional CFO Logo" width={100} height={100} />
-    </div>
-    <div className="text-container">
-      <h1>FRACTIONAL CFO</h1>
-      <p>by MetaCap</p>
-    </div>
-  </div>
+const Footer = () => (
+  <footer className="footer">
+    <Image src="/Fractional CFO Logo.png" alt="Fractional CFO Logo" width={100} height={100} />
+    <a href="#subscribe" className="subscribe-button">SUBSCRIBE</a>
+  </footer>
 );
 
-export default SubHeader;
+export default Footer;
