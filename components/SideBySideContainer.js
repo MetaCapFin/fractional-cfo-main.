ac67793e049da3a382c4d56448dda709ebc0da41
MetaCapFin <advisor@metacap.finance>
2025-02-13 11:45:29 -0500

    Create SideBySideContainer.js

---
+++ components/SideBySideContainer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const SideBySideContainer = () => (
+  <div className="side-by-side-container">
+    <div className="container">Container 1</div>
+    <div className="container">Container 2</div>
+  </div>
+);
+
+export default SideBySideContainer;
