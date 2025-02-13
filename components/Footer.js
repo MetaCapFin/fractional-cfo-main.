04d6a15bfc3e7d0a0e4cf1e75e1c849d8727a837
MetaCapFin <advisor@metacap.finance>
2025-02-13 16:56:40 -0500

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -3,12 +3,7 @@ import React from 'react';
 const Footer = () => (
   <footer className="footer">
     <div className="footer-logo-placeholder">LOGO HERE</div>
-    <nav>
-      <a href="#about-us">About Us</a>
-      <a href="#fractional-cfo-services">Fractional CFO Services</a>
-      <a href="#media">Media</a>
-    </nav>
-    <a href="#subscribe" className="subscribe-button">SUBSCRIBE</a>
+        <a href="#subscribe" className="subscribe-button">SUBSCRIBE</a>
   </footer>
 );
 
