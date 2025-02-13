0353b2a7c38aa1498adff125bf391e3b43385a9f
MetaCapFin <advisor@metacap.finance>
2025-02-13 11:43:51 -0500

    Create Header.js

---
+++ components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => (
+  <header id="header">
+    <nav>
+      <a href="#about-us">About Us</a>
+      <a href="#fractional-cfo-services">Fractional CFO Services</a>
+      <a href="#media">Media</a>
+    </nav>
+    <a href="#contact-us" className="contact-button">CONTACT US</a>
+  </header>
+);
+
+export default Header;
