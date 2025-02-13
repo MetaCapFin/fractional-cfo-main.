0ecd5390e9c3260f4dda42d27c208cdc2fe30e5f
MetaCapFin <advisor@metacap.finance>
2025-02-13 11:46:38 -0500

    Create NewContainer.js

---
+++ components/NewContainer.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const NewContainer = () => (
+  <div className="new-container">
+    <h2>Additional Fractional CFO Services</h2>
+    <div className="new-placeholders">
+      <div className="new-placeholder">
+        <h3>Fractional CFO Onboarding</h3>
+        <ul>
+          <li>Main Feature 1</li>
+          <li>Main Feature 2</li>
+          <li>Main Feature 3</li>
+          <li>Main Feature 4</li>
+        </ul>
+      </div>
+      <div className="new-placeholder">
+        <h3>Fractional CFO Monthly Support</h3>
+        <ul>
+          <li>Main Feature 1</li>
+          <li>Main Feature 2</li>
+          <li>Main Feature 3</li>
+          <li>Main Feature 4</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+);
+
+export default NewContainer;
