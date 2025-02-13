a31c00a64dfb720d14e727ac63e4de2f6a59efea
MetaCapFin <advisor@metacap.finance>
2025-02-13 11:47:14 -0500

    Create AdditionalContainer.js

---
+++ components/AdditionalContainer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const AdditionalContainer = () => (
+  <div className="additional-container">
+    <div className="half-container">
+      <div className="left">About Us</div>
+      <div className="right"></div>
+    </div>
+    <div className="half-container">
+      <div className="left"></div>
+      <div className="right">Intro to Brochure</div>
+    </div>
+    <a href="#download-brochure" className="download-button">DOWNLOAD BROCHURE/GUIDE</a>
+  </div>
+);
+
+export default AdditionalContainer;
