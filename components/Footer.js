32e98dc4cd73952b954b6c9d49009519175a39eb
MetaCapFin <advisor@metacap.finance>
2025-02-13 11:44:28 -0500

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer id="footer">
+    <div className="footer-logo-placeholder">LOGO HERE</div>
+    <nav>
+      <a href="#about-us">About Us</a>
+      <a href="#fractional-cfo-services">Fractional CFO Services</a>
+      <a href="#media">Media</a>
+    </nav>
+    <a href="#subscribe" className="subscribe-button">SUBSCRIBE</a>
+  </footer>
+);
+
+export default Footer;
