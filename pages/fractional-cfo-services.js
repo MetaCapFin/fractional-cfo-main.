8e02f5385783c779be58b31003c83d2ed2e1fc44
MetaCapFin <advisor@metacap.finance>
2025-02-14 11:31:03 -0500

    Create fractional-cfo-services.js

---
+++ pages/fractional-cfo-services.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const FractionalCFO = () => {
+    return (
+        <div>
+            <h1>Fractional CFO Services</h1>
+            <p>Welcome to our Fractional CFO Services page. Here, we offer...</p>
+            {/* Add more content as needed */}
+        </div>
+    );
+};
+
+export default FractionalCFO;
