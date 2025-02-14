72e5c6117ff7bc4a8d6d3a407cc67d4b31efe75b
MetaCapFin <advisor@metacap.finance>
2025-02-14 11:37:46 -0500

    Update fractional-cfo-services.js

--- pages/fractional-cfo-services.js
+++ pages/fractional-cfo-services.js
@@ -3,15 +3,8 @@ import Head from 'next/head';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import SubHeader from '../components/SubHeader';
-import GifPlaceholder from '../components/GifPlaceholder';
-import CatchphraseContainer from '../components/CatchphraseContainer';
-import SideBySideContainer from '../components/SideBySideContainer';
 import MissionContainer from '../components/MissionContainer';
-import StatisticsContainer from '../components/StatisticsContainer';
 import FractionalCFOContainer from '../components/FractionalCFOContainer';
-import NewContainer from '../components/NewContainer';
-import KeyValuesContainer from '../components/KeyValuesContainer';
-import AdditionalContainer from '../components/AdditionalContainer';
 
 const FractionalCFO = () => (
   <div>
